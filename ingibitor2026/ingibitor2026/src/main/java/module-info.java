da30b1d5070b4174296e5bbd7ad4cbf805fbecb2
lizavio <lizaglech@yandex.ru>
2026-02-23 18:09:47 +0300

    Delete ingibitor2026/ingibitor2026 directory

--- ingibitor2026/ingibitor2026/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.ingibitor2026 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-    requires java.desktop;
-
-    opens com.example.ingibitor2026 to javafx.fxml;
-    exports com.example.ingibitor2026;
-}
